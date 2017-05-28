738f4c5b88ca10b0dfac4b2a2143046dde526be3
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-05-28 11:37:10 +0100

    Install nasm.

--- install.bat
+++ install.bat
@@ -1,7 +1,8 @@
 @echo on
 git submodule update --init --recursive --depth 100
-mkdir c:\yasm
-set PATH=c:\yasm;%PATH%
-if "%PLATFORM%" == "x64" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe
-if "%PLATFORM%" == "x86" curl -o "c:\yasm\yasm.exe" http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win32.exe
+if "%PLATFORM%" == "x64" curl -o "c:\nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win64/nasm-2.13.01-win64.zip
+if "%PLATFORM%" == "x86" curl -o "c:\nasm.zip" http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/win32/nasm-2.13.01-win32.zip
+7z x c:\nasm.zip -oc:\
+dir /s /b c:\nasm-2.13.01
+set PATH=c:\nasm-2.13.01;%PATH%
 @echo off
