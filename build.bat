c178a2696a559efdfded12a4c6ad35beca270369
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-12-15 14:57:58 +0000

    Remove unused v141, set up build bat for older toolsets.

--- build.bat
+++ build.bat
@@ -3,14 +3,31 @@ rem set up Visual Studio 2017 64-bit environment for v140 toolset
 rem see https://www.appveyor.com/docs/lang/cpp/
 rem and https://stackoverflow.com/a/46994531
 rem (differenes: no need for SDK, use native amd64 instead of x86_amd64)
+if "%TOOLSET%" == "v90" (
+  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
+  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+)
+if "%TOOLSET%" == "v100" (
+  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
+  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+)
+if "%TOOLSET%" == "v110" (
+  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
+  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+)
+if "%TOOLSET%" == "v120" (
+  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+)
 if "%TOOLSET%" == "v140" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.0
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" -vcvars_ver=14.0
+  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 )
-if "%TOOLSET%" == "v141" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.1
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" -vcvars_ver=14.1
-)
+rem v140 can be consumed by v141 so no point in compiling this
+rem if "%TOOLSET%" == "v141" (
+rem   if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" -vcvars_ver=14.1
+rem   if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" -vcvars_ver=14.1
+rem )
 rem run main build script
 C:\msys64\usr\bin\bash -lc "$APPVEYOR_BUILD_FOLDER/build.sh"
 @echo off
