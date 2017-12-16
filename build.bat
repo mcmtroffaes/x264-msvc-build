470ad6c80c44100799ff2e9a5b479a94be6715e8
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2017-12-16 18:50:39 +0000

    Remove unused toolsets.

--- build.bat
+++ build.bat
@@ -3,18 +3,6 @@ rem set up Visual Studio environment for specified toolset and platform
 rem see https://www.appveyor.com/docs/lang/cpp/
 rem and https://stackoverflow.com/a/46994531
 rem (differenes: no need for SDK, use native amd64 instead of x86_amd64)
-if "%TOOLSET%" == "v90" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-)
-if "%TOOLSET%" == "v100" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
-)
-if "%TOOLSET%" == "v110" (
-  if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" amd64
-  if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-)
 if "%TOOLSET%" == "v120" (
   if "%PLATFORM%" == "x64" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
   if "%PLATFORM%" == "x86" call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
